ca6357a13b7cb39b6b2ea1c921910cc3d6eb5fe6
kelseyberta <ber20002@byui.edu>
2023-04-26 13:25:37 -0400

    API to render

--- swagger.js
+++ swagger.js
@@ -5,17 +5,12 @@ const doc = {
     title: 'My API',
     description: 'Contacts API'
   },
-  host: 'localhost:8080',
-  schemes: ['http']
+  host: 'https://cse341-lesson02-g6sf.onrender.com/',
+  schemes: ['https']
 };
 
 const outputFile = './swagger.json';
 const endpointsFiles = ['./routes/index.js'];
 
-// generate swagger.json
 swaggerAutogen(outputFile, endpointsFiles, doc);
 
-// Run server after it gets generated
-// swaggerAutogen(outputFile, endpointsFiles, doc).then(async () => {
-//   await import('./index.js');
-// });
